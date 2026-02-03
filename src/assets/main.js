38bbffc9234f2a256061a0e32463b0deea66f754
Amr Loksha <amrloksha151@proton.me>
2026-02-03 13:49:01 +0200

    joinAlert

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,10 +2,19 @@ import './bootstrap.min.css';
 import './nav.css';
 import $ from 'jquery';
 import { ImageAnimator } from './modules/animate.js'
+import Alert from "./modules/alerts.js"
 
 function main() {
     const animator = new ImageAnimator("#lineman");
     animator.breathe("41vh", "auto");
+    const joinAlert = new Alert(
+        "📢We are hiring!!📢",
+        "We are preparing for the upcoming year and looking forward to escalate our infrastructure and add more features and capabilities.\nIf you want to become a part of the next season team, navigate to /dev/join.",
+        "info",
+        "warning",
+        "main"
+    );
+    joinAlert.messageAlert();
 }
 
 $(main())
--- src/assets/modules/alerts.js
+++ src/assets/modules/alerts.js
@@ -2,18 +2,31 @@ import * as bootstrap from 'bootstrap';
 import $ from 'jquery';
 
 class Alert {
-    constructor(title, message, color) {
+    constructor(title, message, text_color, bg_color, parent) {
         this.title = title;
         this.message = message;
-        this._color = color;
+        this._color = [text_color, bg_color];
+        this.parent = parent;
     }
 
     get color() {
-        return [`text-${this._color}`, `bg-${this._color}`];
+        return [`text-${this._color[0]}`, `bg-${this._color[1]}`];
     }
 
     set color(color) {
         this._color = color;
+    }
+
+    messageAlert() {
+        
+    }
+
+    alertBox() {
+        
+    }
+
+    popupAlert() {
+        
     }
 }
 
